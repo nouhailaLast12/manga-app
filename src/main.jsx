1ab71e10c75731556102898a5682fda634d190d5
nouhaila <nouha.nouhailoux@gmail.com>
2026-08-21 21:48:35 +0100

    Add global fetch interceptor

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+// Intercept fetch to automatically add CORS proxy to all MangaDex requests
+const originalFetch = window.fetch;
+window.fetch = async function(resource, init) {
+  let url = resource;
+  if (typeof url === 'string' && url.includes('api.mangadex.org') && !url.includes('corsproxy.io')) {
+    url = `https://corsproxy.io/?${url}`;
+  }
+  return originalFetch(url, init);
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
