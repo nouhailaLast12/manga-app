161ceb3536e244c8c70464f88b57e61ddf1d4ad2
nouhaila <nouha.nouhailoux@gmail.com>
2026-08-21 23:26:34 +0100

    Fix MangaDex API with correct encoded corsproxy

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-const originalFetch = window.fetch;
-window.fetch = async function(resource, init) {
-  let url = resource;
-  if (typeof url === 'string' && url.includes('api.mangadex.org')) {
-    url = url.replace('http://', 'https://');
-    if (!url.includes('api.allorigins.win')) {
-     
-      url = `https://api.allorigins.win/raw?url=${url}`;
-    }
-  }
-  return originalFetch(url, init);
-};
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
