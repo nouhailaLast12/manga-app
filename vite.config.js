2ba62958dc44456a2faa1231ef9e70d4fc17e4e5
nouhaila <nouha.nouhailoux@gmail.com>
2026-09-16 17:07:43 +0100

    fix: add BrowserRouter and ErrorBoundary in main.jsx

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // <--- تأكيد مسار الجذر الرئيسي
+  build: {
+    outDir: 'dist',
+    sourcemap: true, // <--- باش يخلينا نشوفو أي خطأ في الكونسول إذا وقع
+  },
 })
